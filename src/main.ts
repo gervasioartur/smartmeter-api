fb33d7a4602da3604e22ffadece4d26b54568ee1
Gervásio Artur Dombo <gervasioartur@outlook.com.br>
2024-08-29 23:18:24 -0300

    feat: add swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = new DocumentBuilder()
+    .setTitle('smartmeasure-api')
+    .setDescription(
+      `serviço que gerencia a leitura individualizada de
+consumo de água e gás. Para facilitar a coleta da informação, o serviço utilizará IA para
+obter a medição através da foto de um medidor`,
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(8080);
 }
 bootstrap();
